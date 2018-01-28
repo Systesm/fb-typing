ec1b5d6ae03682454c4d06a34ab49a9fb7fe5035
Quy Pham <phamhiepdan98@gmail.com>
2018-01-28 10:51:14 +0700

    Remove limit typing & config structure folder

---
+++ es5/index.js
@@ -0,0 +1,79 @@
+"use strict";
+
+var _cheerio = require("cheerio");
+
+var _cheerio2 = _interopRequireDefault(_cheerio);
+
+var _promptInput = require("prompt-input");
+
+var _promptInput2 = _interopRequireDefault(_promptInput);
+
+var _promptPassword = require("prompt-password");
+
+var _promptPassword2 = _interopRequireDefault(_promptPassword);
+
+var _chalk = require("chalk");
+
+var _chalk2 = _interopRequireDefault(_chalk);
+
+var _fbLogin = require("./fbLogin");
+
+var _fbLogin2 = _interopRequireDefault(_fbLogin);
+
+var _typing = require("./typing");
+
+var _getFriends = require("./getFriends");
+
+var _timers = require("timers");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var timeout = function timeout(ms) {
+    return new Promise(function (res) {
+        return setTimeout(res, ms);
+    });
+};
+
+var TypetoFriends = async function TypetoFriends(friends, fb_dtsg, cookies) {
+    for (var i = 0; i < friends.friends.data.length; i++) {
+        var receiver = friends.friends.data[i].id;
+        console.log(_chalk2.default.yellow.bold('[~] ') + _chalk2.default.greenBright('Typing: ') + _chalk2.default.green.bold(friends.friends.data[i].name));
+        (0, _typing.typing)(receiver, fb_dtsg, cookies);
+    }
+    console.log(_chalk2.default.blue.bold('[...] ') + _chalk2.default.yellow.bold('Chờ đợi là hạnh phúc (20s)'));
+    await timeout(25000);
+    TypetoFriends(friends, fb_dtsg, cookies);
+};
+
+(async function () {
+    try {
+
+        console.log(_chalk2.default.blue.bold('Facebook Typing') + _chalk2.default.white(' by ') + _chalk2.default.green.underline.bold('GTFAF (fb.me/GTFAF)'));
+
+        var username = await new _promptInput2.default({
+            name: 'first',
+            message: 'Nhập tài khoản của bạn'
+        }).run();
+
+        var password = await new _promptPassword2.default({
+            type: 'password',
+            message: 'Nhập mật khẩu của bạn',
+            name: 'password'
+        }).run();
+
+        if (typeof username == 'undefined' || username.length < 5 || typeof password == 'undefined' || password.length < 5) throw _chalk2.default.bgRed.white.bold('Tài khoản hoặc mật khẩu vui lòng nhập hơn 5 ký tự');
+
+        var login = await (0, _fbLogin2.default)(username, password);
+        console.log(_chalk2.default.white.bgGreen.bold('Đăng nhập thành công!'));
+        var friends = await (0, _getFriends.getFriends)(login.access_token);
+        console.log(_chalk2.default.bgCyan.white.bold('Lấy danh sách bạn bè...'));
+        var tokenType = await (0, _getFriends.getFb_dtsg)(login.cookies);
+        var $ = _cheerio2.default.load(tokenType);
+        var fb_dtsg = $('[name=fb_dtsg]').val();
+
+        console.log(_chalk2.default.bgBlueBright.white('Bắt đầu soạn tin cho bạn bè...'));
+        await TypetoFriends(friends, fb_dtsg, login.cookies);
+    } catch (error) {
+        console.log(error);
+    }
+})();
---
+++ src/index.js
@@ -0,0 +1,59 @@
+import cheerio from "cheerio"
+import Input from "prompt-input"
+import Password from "prompt-password"
+import chalk from "chalk"
+import fbLogin from "./fbLogin"
+import { typing } from "./typing"
+import { getFriends, getFb_dtsg } from "./getFriends"
+import { setInterval } from "timers"
+
+const timeout = ms => new Promise(res => setTimeout(res, ms))
+
+let TypetoFriends = async (friends, fb_dtsg, cookies) => {
+    for (let i = 0; i < friends.friends.data.length; i++) {
+        let receiver = friends.friends.data[i].id;
+        console.log(chalk.yellow.bold('[~] ') + chalk.greenBright('Typing: ') + chalk.green.bold(friends.friends.data[i].name))
+        typing(receiver, fb_dtsg, cookies)
+    }
+    console.log(chalk.blue.bold('[...] ') + chalk.yellow.bold('Chờ đợi là hạnh phúc (20s)'))
+    await timeout(25000)
+    TypetoFriends(friends, fb_dtsg, cookies)
+}
+
+(async () => {
+    try {
+
+        console.log(chalk.blue.bold('Facebook Typing') + chalk.white(' by ') + chalk.green.underline.bold('GTFAF (fb.me/GTFAF)'));
+
+        let username = await new Input({
+            name: 'first',
+            message: 'Nhập tài khoản của bạn'
+        }).run()
+
+        let password = await new Password({
+            type: 'password',
+            message: 'Nhập mật khẩu của bạn',
+            name: 'password'
+        }).run()
+
+        if (typeof username == 'undefined' ||
+        username.length < 5 ||
+        typeof password == 'undefined' ||
+        password.length < 5)
+            throw chalk.bgRed.white.bold('Tài khoản hoặc mật khẩu vui lòng nhập hơn 5 ký tự')
+
+        let login = await fbLogin(username, password)
+        console.log(chalk.white.bgGreen.bold('Đăng nhập thành công!'))
+        let friends = await getFriends(login.access_token)
+        console.log(chalk.bgCyan.white.bold('Lấy danh sách bạn bè...'))
+        let tokenType = await getFb_dtsg(login.cookies)
+        let $ = cheerio.load(tokenType)
+        let fb_dtsg = $('[name=fb_dtsg]').val()
+
+        console.log(chalk.bgBlueBright.white('Bắt đầu soạn tin cho bạn bè...'))
+        await TypetoFriends(friends, fb_dtsg, login.cookies)
+
+    } catch (error) {
+        console.log(error)
+    }
+})()
